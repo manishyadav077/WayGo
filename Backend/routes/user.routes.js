8e85c20e7757fc5c6682149f8afd1bab55c3897a
manishyadav077 <136573039+manishyadav077@users.noreply.github.com>
2025-01-23 10:36:27 +0545

    user login completed

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -21,4 +21,15 @@ router.post(
   userController.registerUser
 );
 
+router.post(
+  "/login",
+  [
+    body("email").isEmail().withMessage("Invalid Email"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 character long"),
+  ],
+  userController.loginUser
+);
+
 module.exports = router;
