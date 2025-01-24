8f703df7f58f06650c48b17bb6a98c2c959b1d0a
manishyadav077 <136573039+manishyadav077@users.noreply.github.com>
2025-01-24 17:50:03 +0545

    backend completed

--- Backend/routes/ride.routes.js
+++ Backend/routes/ride.routes.js
@@ -37,4 +37,11 @@ router.get(
   rideController.getFare
 );
 
+router.post(
+  "/confirm",
+  authMiddleware.authCaptain,
+  body("rideId").isMongoId().withMessage("Invalid ride id"),
+  rideController.confirmRide
+);
+
 module.exports = router;
