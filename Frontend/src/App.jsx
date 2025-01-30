1b587603613c20b5fa33cbc961c303b070057485
manishyadav077 <136573039+manishyadav077@users.noreply.github.com>
2025-01-30 20:52:23 +0545

    captain login page created

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import CaptainSignup from './pages/CaptainSignup'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import CaptainSignup from "./pages/CaptainSignup";
+import CaptainLogin from "./pages/CaptainLogin";
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path='/captain-signup' element={<CaptainSignup/>}/>
+        <Route path="/captain-register" element={<CaptainSignup />} />
+        <Route path="/captain-login" element={<CaptainLogin />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ Frontend/src/pages/CaptainLogin.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { setFormField, resetForm } from "../store/captainAuthSlice";
+
+import axios from "axios";
+import Input from "../component/Input";
+
+const CaptainLogin = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { email, password } = useSelector((state) => state.captainAuth);
+
+  const handleInputChange = (field, value) => {
+    const updatedValue = field === "vehicleCapacity" ? Number(value) : value;
+    dispatch(setFormField({ field, value: updatedValue }));
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    const captainData = {
+      email,
+      password,
+    };
+
+    const response = await axios.post("/api/captain/login", captainData);
+    if (response.status === 201) {
+      const data = response.data;
+      localStorage.setItem("token", data.token);
+      dispatch(resetForm());
+      navigate("/captain-home");
+    }
+  };
+
+  return (
+    <div className="py-5 px-5 h-screen flex flex-col justify-between">
+      <div>
+        <img
+          className="w-20 mb-3"
+          src="https://www.svgrepo.com/show/505031/uber-driver.svg"
+          alt=""
+        />
+
+        <form onSubmit={submitHandler}>
+          <h3 className="text-lg w-full font-medium mb-2">
+            What's our Captain's name
+          </h3>
+
+          <Input
+            label="Email"
+            type="email"
+            placeholder="email@example.com"
+            value={email}
+            onChange={(e) => handleInputChange("email", e.target.value)}
+            required
+            className="mb-7"
+          />
+
+          <Input
+            label="Password"
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => handleInputChange("password", e.target.value)}
+            required
+            className="mb-7"
+          />
+
+          <button className="bg-[#111] text-white font-semibold mb-3 rounded-lg px-4 py-2 w-full text-lg">
+           Login
+          </button>
+        </form>
+
+        <p className="text-center">
+          Don't have an account?{" "}
+          <Link to="/captain-login" className="text-blue-600">
+            Register
+          </Link>
+        </p>
+      </div>
+      <div>
+        <p className="text-[10px] mt-6 leading-tight">
+          This site is protected by reCAPTCHA and the{" "}
+          <span className="underline">Google Privacy Policy</span> and{" "}
+          <span className="underline">Terms of Service apply</span>.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default CaptainLogin;
