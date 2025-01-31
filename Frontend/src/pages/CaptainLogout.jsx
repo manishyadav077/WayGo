38ab36b2dd71e940e69acf2bb2772e2c970d3e60
manishyadav077 <136573039+manishyadav077@users.noreply.github.com>
2025-01-31 17:24:50 +0545

    captain logout created

---
+++ Frontend/src/pages/CaptainLogout.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const CaptainLogout = () => {
+  const token = localStorage.getItem("captain-token");
+  const navigate = useNavigate();
+
+  axios
+    .get("/api/captains/logout", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((response) => {
+      if (response.status === 200) {
+        localStorage.removeItem("captain-token");
+        navigate("/captain-login");
+      }
+    });
+  return <div>CaptainLogout</div>;
+};
+
+export default CaptainLogout;
